3f6e34214d50e54d2e9f58d84210c90ebe907b19
Olivia <okarney@udel.edu>
2024-06-28 14:27:42 -0400

    working on govpositions buttons

--- frontend_elections/screens/GovPositions.tsx
+++ frontend_elections/screens/GovPositions.tsx
@@ -1,11 +1,15 @@
 import React, {FunctionComponent, useState} from "react";
-import {StatusBar} from "expo-status-bar";
+import {StyleSheet} from "react-native";
+import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
+
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
 
 //custom components
 import { Container } from "../components/shared";
 import { colors } from "../components/colors";
 import RegularText from "../components/texts/RegularText";
+import RegularButton from "../components/buttons/RegularButton";
 
 
 const GovPositionsContainer = styled(Container)`
@@ -15,15 +19,63 @@ const GovPositionsContainer = styled(Container)`
 
 `;
 
+const TopPadding = styled.View`
+    width: 100%;
+    height: 15%;
+`;
+
+const Content = styled.View`
+    justify-content: center;
+    align-items: center;
+    flexDirection: row;
+    width: 90%;
+    height: 60%;
+    flex: 1;
+`;
 
 
-const GovPositions: FunctionComponent = () => {
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+      },
+    button: {
+        backgroundColor: '#3498db',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    buttonText: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+  });
+
+
+interface GovPositionProps {
+    navigation: NavigationProp<ParamListBase>;
+}
+
+const GovPositions: FunctionComponent<GovPositionProps> = ({navigation}) => {
     return (
         <>
             <StatusBar style="dark"/>
 
             <GovPositionsContainer>
-                <RegularText>Hello</RegularText>
+                <TopPadding></TopPadding>
+                
+                <Content>
+                    <RegularButton onPress={() => navigation.navigate('GovPositions')}>Login</RegularButton>
+                    
+
+                    <RegularButton onPress={() => navigation.navigate('GovPositions')}>Login</RegularButton>
+                    
+                    <RegularButton onPress={() => navigation.navigate('GovPositions')}>Login</RegularButton>
+
+                </Content>
 
             </GovPositionsContainer>
         </> 
